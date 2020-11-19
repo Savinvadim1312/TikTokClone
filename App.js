893bd630d10d01cc0422d1781dbfe5a108039ce8
Vadim Savin <savinvadim1312@gmail.com>
2020-11-19 17:57:54 +0200

    connect Cognito user with Database user

--- App.js
+++ App.js
@@ -7,17 +7,62 @@
  */
 
 import 'react-native-gesture-handler';
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
 
 import Navigation from './src/navigation';
 
+import {Auth, API, graphqlOperation} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 
+import {createUser} from './src/graphql/mutations';
+import {getUser} from './src/graphql/queries';
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)];
+};
+
 const App: () => React$Node = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get currently authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      if (!userInfo) {
+        return;
+      }
+
+      // check if THE user exist in database
+      const getUserResponse = await API.graphql(
+        graphqlOperation(getUser, {id: userInfo.attributes.sub}),
+      );
+
+      if (getUserResponse.data.getUser) {
+        console.log('User already exists in database');
+        return;
+      }
+
+      // if it doesn't (it's newly registered user)
+      // then, create a new user in database
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage(),
+      };
+
+      await API.graphql(graphqlOperation(createUser, {input: newUser}));
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
