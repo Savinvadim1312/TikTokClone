ba16846af79b4b508d5be48bcc755a0a53c394dc
Vadim Savin <savinvadim1312@gmail.com>
2020-11-19 17:38:45 +0200

    auth

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ import {
 
 import Navigation from './src/navigation';
 
+import {withAuthenticator} from 'aws-amplify-react-native';
+
 const App: () => React$Node = () => {
   return (
     <>
@@ -26,4 +28,4 @@ const App: () => React$Node = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
